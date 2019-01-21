b6a40c507657de669cfadc46938af30eaea0e2b6
anangnov99@gmail.com <anangnov99@gmail.com>
2019-01-21 22:56:05 +0700

    koptimaal

--- app/Http/Controllers/HomesController.php
+++ app/Http/Controllers/HomesController.php
@@ -88,8 +88,12 @@ class HomesController extends Controller
             return $item;
         }); 
 
-        $command = escapeshellcmd('/Project-Ku/rekomendasi-film(python)/kf_cross.py');
-        $exec = shell_exec('python '.$command);
+        $countTableKOptimal = DB::table('k_optimals')->count();
+        
+        if($countTableKOptimal < 1) {
+            $command = escapeshellcmd('/Project-Ku/rekomendasi-film(python)/kf_cross.py');
+            $exec = shell_exec('python '.$command);
+        }
 
         $getCosine = DB::select('SELECT c_products.id_film, (c_products.tf_idf / d_products.tf_idf) AS total_cosine FROM c_products JOIN d_products ON c_products.id_film = d_products.id_film ORDER by total_cosine DESC');
         $getCosine = collect($getCosine);
